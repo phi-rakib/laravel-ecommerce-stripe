cba2d45fe8cb8aa7344aacad57bf1a9b4a5571f3
rakib <phi.rakib@gmail.com>
2024-06-27 12:07:43 +0600

    added product, cart & checkout controllers routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
+Route::get('product/{id}', [ProductController::class, 'show'])->name('products.show');
+Route::post('cart/add', [CartController::class, 'add'])->name('carts.add');
+Route::get('cart', [CartController::class, 'index'])->name('carts.index');
+Route::get('checkout', [PaymentController::class, 'checkout'])->name('payments.checkout');
+Route::post('create-checkout-session', [PaymentController::class, 'createCheckoutSession'])->name('payments.createCheckoutSession');
+Route::get('success', [PaymentController::class, 'success'])->name('payments.success');
+Route::get('cancel', [PaymentController::class, 'cancel'])->name('payments.cancel');
